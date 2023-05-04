a93f7f1c27d18d053c3a5b878f9d26172204b832
Lorena <lorenacapraru@gmail.com>
2023-05-04 13:47:52 +0100

    Timeline

--- lorenaportofolio/src/Timeline.jsx
+++ lorenaportofolio/src/Timeline.jsx
@@ -4,8 +4,35 @@ import "./Timeline.css";
 
 export const Timeline = () => {
   return (
-    <div className="timeline">
-      Timeline
+    <div className="design-section">
+      <div className="timeline">
+        <div className="timeline-empty"></div>
+        <div className="timeline-middle">
+          <div className="timeline-circle"></div>
+        </div>
+        <div className="timeline-component timeline-content">
+          <h3>HTML</h3>
+          <p>Some Text</p>
+        </div>
+        <div className="timeline-component timeline-content">
+          <h3>CSS</h3>
+          <p>Some Text.</p>
+        </div>
+        <div className="timeline-middle">
+          <div className="timeline-circle"></div>
+        </div>
+        <div className="timeline-empty"></div>
+
+        <div className="timeline-empty"></div>
+
+        <div className="timeline-middle">
+          <div className="timeline-circle"></div>
+        </div>
+        <div className=" timeline-component timeline-content">
+          <h3>Javascript</h3>
+          <p>Some Text.</p>
+        </div>
+      </div>
       <Footer />
     </div>
   );
