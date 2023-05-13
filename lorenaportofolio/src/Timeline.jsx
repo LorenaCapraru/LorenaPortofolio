5724f095fb85912387eb36f51553098a83f9a868
Lorena <lorenacapraru@gmail.com>
2023-05-13 15:23:14 +0100

    Update Timeline.jsx

--- lorenaportofolio/src/Timeline.jsx
+++ lorenaportofolio/src/Timeline.jsx
@@ -20,7 +20,7 @@ export const Timeline = () => {
                 <circle class="stroke" cx="60" cy="60" r="50" />
               </svg>
             </div>
-            <h3>lallilifdda</h3>
+            <h3>CSS</h3>
           </div>
         </a>
         <div class="card">
