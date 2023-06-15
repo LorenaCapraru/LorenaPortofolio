8fdbb44373fd0a068c9cb60a8b8339f9f678100b
Lorena <lorenacapraru@gmail.com>
2023-06-15 23:24:32 +0100

    Update Timeline.jsx

--- lorenaportofolio/src/Timeline.jsx
+++ lorenaportofolio/src/Timeline.jsx
@@ -36,7 +36,7 @@ export const Timeline = () => {
           </div>
         </div>
         <div class="card">
-          <h3 class="title">JavaaaScript</h3>
+          <h3 class="title">JavaaffaScript</h3>
           <div class="bar">
             <div class="emptybar"></div>
             <div class="filledbar"></div>
